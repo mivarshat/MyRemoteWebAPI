82b5734eccaf7f103033264334a41416d699609a
miVarsha <varsha.s.tavade@gmail.com>
2022-10-12 14:27:48 +0530

    End of section 2

---
+++ WebApplication1/WebApplication1/Data/WalkDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models.Domain;
+
+namespace WebApplication1.Data
+{
+    public class WalkDBContext:DbContext 
+    {
+        public WalkDBContext(DbContextOptions<WalkDBContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Region> Regions { get; set; }
+
+        public DbSet<Walk> Walks { get; set; }
+
+        public DbSet<WalkDifficulty> WalkDifficulties { get; set; }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Domain/WalkDifficulty.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication1.Models.Domain
+{
+    public class WalkDifficulty
+    {
+        public Guid Id { get; set; }
+
+        public string Code { get; set; }
+
+        //Navigation Property
+        
+    }
+}
